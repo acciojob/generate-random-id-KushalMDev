c2974b22482f799195b9a6ced58971d8b155db67
KushalMDev <105456987+KushalMDev@users.noreply.github.com>
2024-03-25 16:49:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function makeid(l) {
   // write your code here
+	// our code here
+    let arr=[65,97]
+    let digits=[0,1,2,3,4,5,6,7,8,9]
+    let res=''
+    for(let j=0;j<l;j++){
+        let random=parseInt(Math.random()*26)
+        // console.log(random);
+        if(random<9){
+           let num =digits[random]
+           res+=num
+        }
+        else{
+            let ind=parseInt(Math.random()*2)
+            let c=String.fromCharCode(arr[ind]+random)
+            res+=c;
+        }
+    }
+    return res
 }
 
 // Do not change the code below.
